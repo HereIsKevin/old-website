7aa88a80b722664bcf5b4373641787f7e31be872
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2023-06-24 02:00:01 -0700

    🚀 Automatically inline critical CSS

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config"
 
 import compress from "astro-compress"
+import critters from "astro-critters"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress()],
+  integrations: [compress(), critters()],
 })
