c4f1a024918bea96b3311bffdd7e1739f4de61bf
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2023-06-25 12:29:45 -0700

    🔧 Add site to config

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config"
 
 export default defineConfig({
+  site: "https://hereiskevin.github.io",
   compressHTML: true,
   build: {
     inlineStylesheets: "auto",
