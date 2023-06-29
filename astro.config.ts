0a730d64a35efc5fe28d43047b1c48c89bd47b22
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2023-06-28 17:16:49 -0700

    🚀 Add sitemaps

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config"
 
+import sitemap from "@astrojs/sitemap"
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://hereiskevin.github.io",
   compressHTML: true,
   build: {
     inlineStylesheets: "auto",
   },
+  integrations: [sitemap()],
   vite: {
     build: {
       target: "modules",
