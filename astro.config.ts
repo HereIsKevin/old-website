26998ad06dcae27adc8ae6799ebf3e657a85cc56
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2023-07-01 20:10:22 -0700

    🔧 Experimental assets

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config"
 
 import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,10 +10,13 @@ export default defineConfig({
   build: {
     inlineStylesheets: "auto",
   },
-  integrations: [sitemap()],
+  integrations: [sitemap(), tailwind()],
   vite: {
     build: {
       target: "modules",
     },
+  },
+  experimental: {
+    assets: true,
   },
 })
